642206904af49f62e1e640041e89fd127cef8d1f
yanisleidi <yaroga88@yahoo.com>
2017-10-26 11:30:46 -0700

    Install and config grunt-contrib-cssmin grunt-contrib-htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,105 @@
 module.exports = function(grunt) {
     grunt.initConfig({
 
-        responsive_images: {
-            dev: {
-                options: {
-                    engine: 'im',
-                    sizes: [{
-                            width: 100,
-                            height: 50,
-                            suffix: '_1x',
-                            quality: 75
-                        }, {
-                            width: 200,
-                            height: 100,
-                            suffix: '_2x',
-                            quality: 75
-                        },
-                        {
-                            width: 140,
-                            height: 100,
-                            suffix: 'pizza',
-                            quality: 75
-                        }
-                    ]
-                },
+        // responsive_images: {
+        //     dev: {
+        //         options: {
+        //             engine: 'im',
+        //             sizes: [{
+        //                     width: 100,
+        //                     height: 50,
+        //                     suffix: '_1x',
+        //                     quality: 75
+        //                 }, {
+        //                     width: 200,
+        //                     height: 100,
+        //                     suffix: '_2x',
+        //                     quality: 75
+        //                 },
+        //                 {
+        //                     width: 140,
+        //                     height: 100,
+        //                     suffix: 'pizza',
+        //                     quality: 75
+        //                 }
+        //             ]
+        //         },
 
+        //         files: [{
+        //             expand: true,
+        //             src: ['*.{gif,jpg,png}'],
+        //             cwd: 'img-copy/',
+        //             dest: 'images/'
+        //         }]
+        //     }
+        // },
+        cssmin: {
+            options: {},
+            target: {
+                files: [{
+                        expand: true,
+                        cwd: 'css/',
+                        src: ['*.css', '!*.min.css'],
+                        dest: 'css/',
+                        ext: '.min.css'
+                    },
+                    {
+                        expand: true,
+                        cwd: './views/css/',
+                        src: ['*.css', '!*.min.css'],
+                        dest: './views/css/',
+                        ext: '.min.css'
+                    }
+                ]
+            }
+        },
+
+        htmlmin: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true
+            },
+            target: {
                 files: [{
                     expand: true,
-                    src: ['*.{gif,jpg,png}'],
-                    cwd: 'img-copy/',
-                    dest: 'images/'
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: './',
+                    exp: '.min.html'
+                }, {
+                    expand: true,
+                    cwd: './views/',
+                    src: ['*.html'],
+                    dest: './views',
+                    exp: '.min.html'
                 }]
             }
-        },
+        }
 
-        clean: {
-            dev: {
-                src: ['images']
-            }
+        // clean: {
+        //     dev: {
+        //         src: ['images']
+        //     }
 
-        },
+        // },
 
-        mkdir: {
-            dev: {
-                options: {
-                    create: ['images'],
-                }
-            }
-        },
+        // mkdir: {
+        //     dev: {
+        //         options: {
+        //             create: ['images'],
+        //         }
+        //     }
+        // },
     });
 
-    grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-mkdir');
+    //grunt.loadNpmTasks('grunt-responsive-images');
+    //grunt.loadNpmTasks('grunt-contrib-clean');
+    //grunt.loadNpmTasks('grunt-mkdir');
     //grunt.loadNpmTasks('grunt-resize-crop');
-    grunt.loadNpmTasks('imagemagick');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
+    //grunt.loadNpmTasks('imagemagick');
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.registerTask('default', ['cssmin', ]);
 
 };
